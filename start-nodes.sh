75cd37c1203b6a012f4a72e90becd58ca0737bb6
BCTS <46267529+BCTSAG@users.noreply.github.com>
2021-02-01 17:06:39 +0100

    Update start-nodes.sh

--- start-nodes.sh
+++ start-nodes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-geth --datadir node1/ --syncmode 'full' --port 30311 --rpc --rpcaddr 'http://127.0.0.1' --rpccorsdomain "*" --rpcport 8540 --rpcapi 'personal,eth,net,web3,txpool,miner' --bootnodes 'enode://79f27a4562c15222cc74f9832099dbe4a6e34aebbc7f61c8cd172d1142ece285f0ca2349c34c6512c2b6432349be41fd5e33622aec0a71cba0ebc97d8081eebc@127.0.0.1:30312' --networkid 99 --gasprice '1' 
+screen -S node1 geth --datadir node1/ --syncmode 'full' --port 30311 --rpc --rpcaddr 'localhost' --rpcport 8540 --rpcapi 'personal,eth,net,web3,txpool,miner' --networkid 99 --gasprice '1'
 
 
