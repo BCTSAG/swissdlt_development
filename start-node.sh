887a91605379c0e1b38d81313e27ad88bd5d29e4
BCTS <46267529+BCTSAG@users.noreply.github.com>
2021-02-11 15:35:15 +0100

    Update start-node.sh

--- start-node.sh
+++ start-node.sh
@@ -1,3 +1,3 @@
 !/bin/bash
 #this sets up a node running in the background with screen
-screen -S node1 geth --datadir node1/ --syncmode 'full' --port 30311 --rpc --rpcaddr 'localhost' --rpcport 8540 --rpcapi 'personal,eth,net,web3,txpool,miner' --networkid 99 --gasprice '1'
+screen -S node1 geth --datadir node1/ --syncmode 'full' --port 30311 --rpc --rpcaddr 'localhost' --rpcport 8545 --rpcapi 'personal,eth,net,web3,txpool,miner' --networkid 99 --gasprice '1'
